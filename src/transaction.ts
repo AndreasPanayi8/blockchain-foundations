ad3f623224b101d416e4e5c29310d3f1ac1c2e70
Mike Vita <michalisvita@mhnyma.com>
2026-03-23 21:32:27 +0200

    Implemented get_transaction

--- src/transaction.ts
+++ src/transaction.ts
@@ -50,40 +50,32 @@ export async function verifyTransaction(node_id : string, socket : Socket, t : T
     }
 
     try {
-      const inObj = await objectManager.get(txid);
+      const inTransaction = await get_transaction(node_id, socket, txid);
 
-      switch (inObj.type) {
-        case 'transaction':
-          if (input.outpoint.index >= inObj.outputs.length) {
-            await send_error(node_id, socket, 'INVALID_TX_OUTPOINT', 'The transaction outpoint index is too large');
-            return false;
-          }
-          const output = inObj.outputs[input.outpoint.index];
-          if (output === undefined) {
-            await send_error(node_id, socket, 'INVALID_FORMAT', 'Referenced output is undefined');
-            return false;
-          }
-
-          const verify = await verifyAsync(
-            hexToBytes(input.sig),
-            utf8ToBytes(canonicalized),
-            hexToBytes(output.pubkey)
-          );
-          
-          if (!verify) {
-            await send_error(node_id, socket, 'INVALID_TX_SIGNATURE', 'Invalid signature');
-            return false;
-          }
-
-          inputSum += output.value;
-          break;
-
-        case 'block':
-          await send_error(node_id, socket, 'INVALID_FORMAT', 'Transaction txid is a block id');
-          return false;
+      if (input.outpoint.index >= inTransaction.outputs.length) {
+        await send_error(node_id, socket, 'INVALID_TX_OUTPOINT', 'The transaction outpoint index is too large');
+        return false;
       }
-    } catch (err) { 
-      console.error(`[${node_id}]: get failed`, err);
+      const output = inTransaction.outputs[input.outpoint.index];
+      if (output === undefined) {
+        await send_error(node_id, socket, 'INVALID_FORMAT', 'Referenced output is undefined');
+        return false;
+      }
+
+      const verify = await verifyAsync(
+        hexToBytes(input.sig),
+        utf8ToBytes(canonicalized),
+        hexToBytes(output.pubkey)
+      );
+      
+      if (!verify) {
+        await send_error(node_id, socket, 'INVALID_TX_SIGNATURE', 'Invalid signature');
+        return false;
+      }
+
+      inputSum += output.value;
+    } catch { 
+      return false;
     }
   }
 
@@ -98,3 +90,25 @@ export async function verifyTransaction(node_id : string, socket : Socket, t : T
   }
   return true
 }
+
+export async function get_transaction(node_id : string, socket : Socket, txid: string) : Promise<Transaction> {
+  const found = await objectManager.exists(txid);
+  if(!found) {
+    await send_error(node_id, socket, 'UNFINDABLE_OBJECT', 'Transaction txid not found in database');
+    throw new Error();
+  }
+
+  try {
+    const obj = await objectManager.get(txid);
+  
+    if (obj.type === 'block') {
+      await send_error(node_id, socket, 'INVALID_FORMAT', 'Transaction txid is a block id');
+      throw new Error();
+    }
+
+    return obj;
+  } catch (err) { 
+    console.error(`[${node_id}]: get failed`, err);
+    throw new Error();
+  }
+} 
