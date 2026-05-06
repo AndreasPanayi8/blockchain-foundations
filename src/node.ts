022a1d5fe56bed628f964ec49fffefcdfacf34b4
Mike Vita <michalisvita@mhnyma.com>
2026-05-06 22:23:04 +0300

    Connecting to a bootstrap node

--- src/node.ts
+++ src/node.ts
@@ -292,6 +292,19 @@ async function connect_to_random_discovered_peer() {
 // Start the server
 server.listen(PORT, async() => {
     console.log(`Server listening on port ${PORT}`)
+    const outbound = new Socket()
+
+    outbound.connect(18018, '95.179.158.137', async () => {
+        const id = `${outbound.remoteAddress}:${outbound.remotePort}`
+        console.log(`Outbound connected to 95.179.158.137:18018 (${id})`)
+
+        attach_handlers(outbound, id)
+        await connect(outbound)
+    })
+
+    outbound.on('error', (err) => {
+        console.error(`Error connecting to peer 95.179.158.137:18018:`, err)
+    })
     await connect_to_random_discovered_peer()
     await initializeMempoolStateFromChainTip(chain_data.get_chaintip());
 })
